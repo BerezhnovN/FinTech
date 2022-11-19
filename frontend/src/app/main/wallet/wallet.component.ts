56cd0a2c69bdc7235d3c528f081e207db44f8d57
Nikita Berezhnov <berezhnov.nikita@wb.ru>
2022-11-19 13:20:07 +0300

    feed: add main module

---
+++ frontend/src/app/main/wallet/wallet.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wallet',
+  templateUrl: './wallet.component.html',
+  styleUrls: ['./wallet.component.scss']
+})
+export class WalletComponent  {
+
+  constructor() { }
+}
