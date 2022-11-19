886f30283bf9260946cafd9dfce829d1a9a06fd2
Nikita Berezhnov <berezhnov.nikita@wb.ru>
2022-11-19 22:38:18 +0300

    feed: добавил интерсептор и guard

---
+++ frontend/src/app/guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { map, Observable } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authSrv: AuthService, private router: Router) {}
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    return this.authSrv.isAuthorized().pipe(
+      map(isAuth => {
+        if (isAuth) {
+          return true;
+        } else {
+          this.router.navigate(['/login']);
+          return false;
+        }
+      })
+    );
+  }
+}
