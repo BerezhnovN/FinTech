1900cc2ea4a4869f220a35a7d1878aa34269bbce
Nikita Berezhnov <berezhnov.nikita@wb.ru>
2022-11-19 15:56:39 +0300

    добавил несколько компонентов

--- frontend/src/app/main/main.module.ts
+++ frontend/src/app/main/main.module.ts
@@ -5,9 +5,11 @@ import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { WalletComponent } from './wallet/wallet.component';
 import { CurrencyComponent } from './currency/currency.component';
+import { ExchangeComponent } from './exchange/exchange.component';
+import { OperationHistoryComponent } from './operation-history/operation-history.component';
 
 @NgModule({
-  declarations: [MainComponent, WalletComponent, CurrencyComponent],
+  declarations: [MainComponent, WalletComponent, CurrencyComponent, ExchangeComponent, OperationHistoryComponent],
   imports: [CommonModule, MainRoutingModule],
 })
 export class MainModule {}
