21bf34658e55175fdd9d4edcd5a73be1c3a1e542
komediantto <komediantto@yandex.ru>
2022-11-19 23:58:36 +0200

    Создал ещё моделей

--- backend/fintech/purchase/models.py
+++ backend/fintech/purchase/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from users.models import User
+from .validators import validate_positive
 
-# Create your models here.
+
+class Active(models.Model):
+    owner = models.ManyToManyField(User,
+                                   through='ActiveOwner',
+                                   verbose_name='Держатель')
+    name = models.CharField(verbose_name='Название актива',
+                            max_length=300)
+    price = models.FloatField(validators=[validate_positive],
+                              verbose_name='Цена')
+
+
+class ActiveOwner(models.Model):
+    active = models.ForeignKey(Active,
+                               on_delete=models.PROTECT,
+                               related_name='active_owner',
+                               verbose_name='Active')
+    owner = models.ForeignKey(User,
+                              on_delete=models.CASCADE,
+                              related_name='owner_active',
+                              verbose_name='Держатель')
+    amount = models.FloatField(validators=[validate_positive],
+                               verbose_name='Количество')
