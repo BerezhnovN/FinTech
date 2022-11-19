6ed9d96be0d51ac4d8c592cb580960e1631c8c1a
DaoNik <nikitataranin@gmail.com>
2022-11-19 13:46:52 +0300

    feat: добавил header и роутинг в main модуль

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // { path: 'login', component: LoginComponent, data: { title: 'Авторизация' } },
   {
-    path: 'main',
+    path: '',
     loadChildren: () => import('./main/main.module').then(m => m.MainModule),
   },
+  // { path: 'login', component: LoginComponent, data: { title: 'Авторизация' } },
   // { path: '**', component: _ },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/src/app/main/main.module.ts
+++ frontend/src/app/main/main.module.ts
@@ -5,9 +5,12 @@ import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { WalletComponent } from './wallet/wallet.component';
 import { CurrencyComponent } from './currency/currency.component';
+import { PaymentsComponent } from './payments/payments.component';
+import { HistoryComponent } from './history/history.component';
+import { BirgeComponent } from './birge/birge.component';
 
 @NgModule({
-  declarations: [MainComponent, WalletComponent, CurrencyComponent],
+  declarations: [MainComponent, WalletComponent, CurrencyComponent, PaymentsComponent, HistoryComponent, BirgeComponent],
   imports: [CommonModule, MainRoutingModule],
 })
 export class MainModule {}
