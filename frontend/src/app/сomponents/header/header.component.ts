27fef670261ffb71c79d6633c218da13600274d2
Nikita Berezhnov <berezhnov.nikita@wb.ru>
2022-11-20 09:29:33 +0300

    feed: добавил выход из аккаунта

--- frontend/src/app/сomponents/header/header.component.ts
+++ frontend/src/app/сomponents/header/header.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../auth/auth.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  onLogout(): void {}
+  constructor(private authSrv: AuthService) {}
+  onLogout(): void {
+    this.authSrv.logout();
+  }
 }
